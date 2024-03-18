1d0b54045fa2c96206ad98cfd30431a885523303
James Grom <jgrom@scu.edu>
2024-03-17 17:20:37 -0700

    uncomment windows focused eye_tracking_settings.py (should allow config to work for windows)

--- eye_tracking_settings.py
+++ eye_tracking_settings.py
@@ -5,7 +5,7 @@ from talon_plugins import speech, eye_mouse, eye_zoom_mouse
 if app.platform == "mac":
     eye_zoom_mouse.config.screen_area = Point2d(300, 175)
     eye_zoom_mouse.config.img_scale = 3 
-# elif app.platform == "windows":
-#     eye_zoom_mouse.config.screen_area = Point2d(200, 150)
-#     eye_zoom_mouse.config.img_scale = 4.5
+elif app.platform == "windows":
+    eye_zoom_mouse.config.screen_area = Point2d(200, 150)
+    eye_zoom_mouse.config.img_scale = 4.5
 
